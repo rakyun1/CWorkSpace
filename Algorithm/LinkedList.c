f55a602731f901d20bd5d2d8ad7c54a16e6dab8d
rakyun1 <dbskim234@naver.com>
2024-04-08 21:22:03 +0900

    LikedList 진행중

--- Algorithm/LinkedList.c
+++ Algorithm/LinkedList.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tagNode
+{
+    int data;
+    struct tagNode* nextNode;
+} Node;
+
+int main(void)
+{
+    Node node = {1, NULL};
+
+    Node* nodep;
+    nodep = (Node*)malloc(10);
+    nodep[0].data = 1;
+    nodep->nextNode = NULL;
+
+    printf("%d\n", sizeof(node));
+    printf("%d\n", sizeof(nodep));
+
+    return 0;
+}
