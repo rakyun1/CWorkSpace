43c1a06c5d77364113bfeb9794cd575ced36fb9d
rakyun1 <dbskim234@naver.com>
2024-04-12 20:15:52 +0900

    링크드 리스트

--- Algorithm/LinkedList.c
+++ Algorithm/LinkedList.c
@@ -7,17 +7,99 @@ typedef struct tagNode
     struct tagNode* nextNode;
 } Node;
 
+Node* SLL_CreateNode(int NewData);
+void SLL_AppendNode(Node** Head, Node* NewNode);
+Node* SLL_GetNodeAt(Node* Head, int Location);
+void SLL_DistroyNode(Node* Node);
+void SLL_RemoveNode(Node** Head, Node* Remove);
+
 int main(void)
 {
-    Node node = {1, NULL};
+    Node* List = NULL;
+    Node* NewNode = NULL;
 
-    Node* nodep;
-    nodep = (Node*)malloc(10);
-    nodep[0].data = 1;
-    nodep->nextNode = NULL;
+    NewNode = SLL_CreateNode(11);
+    SLL_AppendNode(&List, NewNode);
 
-    printf("%d\n", sizeof(node));
-    printf("%d\n", sizeof(nodep));
+
+    NewNode = SLL_CreateNode(112);
+    SLL_AppendNode(&List, NewNode);
+
+    printf("Head :: %d\n", List->data);
+
+    Node* MyNode = NULL;
+    MyNode = SLL_GetNodeAt(List, 1);
+    
+    printf("탐색 된 노드의 데이터 :: %d\n", MyNode->data);
+
+    SLL_RemoveNode(&List, MyNode);
+    SLL_DistroyNode(MyNode);
+    
 
     return 0;
+}
+
+Node* SLL_CreateNode(int NewData)
+{
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+
+    NewNode->data = NewData;
+    NewNode->nextNode = NULL;
+    return NewNode;
+}
+
+void SLL_DistroyNode(Node* Node)
+{
+    free(Node);
+}
+
+void SLL_AppendNode(Node** Head, Node* NewNode)
+{
+    if ((*Head) == NULL)
+    {
+        *Head = NewNode;
+    } 
+    else 
+    {
+        Node* Tail = (*Head);
+        while( Tail->nextNode != NULL)
+        {
+            Tail = Tail->nextNode;
+        }
+        Tail->nextNode = NewNode;
+    }
+}
+
+
+Node* SLL_GetNodeAt(Node* Head, int Location)
+{
+    Node* Current = Head;
+
+    while (Current != NULL && (--Location) >= 0)
+    {
+        Current = Current->nextNode;
+    }
+    return Current;
+}
+
+void SLL_RemoveNode(Node** Head, Node* Remove)
+{
+    if (*Head == Remove)
+    {
+        *Head = Remove->nextNode;
+    }
+    else
+    {
+        Node* Current = *Head;
+        while(Current != NULL && Current->nextNode != Remove)
+        {
+            Current = Current->nextNode;
+        }
+
+        if (Current != NULL)
+        {
+            Current->nextNode = Remove->nextNode;
+        }
+        
+    }
 }
--- PracticeC/extern.c
+++ PracticeC/extern.c
@@ -1,1 +1,13 @@
-int global_var = 0;
+#include <stdio.h>
+
+void fun(int **p) {
+    printf("ptr이 가리키는 대상의 주소: %d\n", **p);
+}
+
+int main() {
+    int x = 10;
+    int *ptr = &x;
+    fun(&ptr); // ptr의 주소를 fun 함수에 전달
+
+    return 0;
+}
