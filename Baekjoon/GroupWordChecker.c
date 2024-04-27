23a5e57dba04ba5e2b3a469231672c45d5f274a2
rakyun1 <dbskim234@naver.com>
2024-04-27 17:27:29 +0900

    Baekjoon 1316 : not yet

---
+++ Baekjoon/GroupWordChecker.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int checkWord(char* word)
+{
+    for (int i = 0; i < sizeof word / sizeof word[0]; i++)
+    {
+        printf("%c ", word[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+int main(void)
+{
+    int answer = 0;
+    int wordCount;
+    scanf("%d", &wordCount);
+
+    char* word = (char*)malloc(sizeof(char) * 101);
+
+    for (size_t i = 0; i < wordCount; i++)
+    {
+        scanf("%s", word);
+        if (checkWord(word))
+        {
+            answer++;
+        }
+        
+    }
+    
+    return 0;
+}
