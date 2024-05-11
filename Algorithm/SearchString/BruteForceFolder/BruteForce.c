5e4fb0a18d1819817e4e05554a290f3f962f486f
rakyun1 <dbskim234@naver.com>
2024-05-11 17:02:51 +0900

    Implement BruteFoce Algorithm

---
+++ Algorithm/SearchString/BruteForceFolder/BruteForce.c
@@ -0,0 +1,26 @@
+#include "BruteForce.h"
+
+
+int BruteForce(char* Text, int TextSize, int Start, char* Pattern, int PatternSize)
+{
+    int i,j = 0;
+
+    for ( i = Start; i <= TextSize - PatternSize; i++)
+    {
+        for ( j = 0; j < PatternSize; j++)
+        {
+            if (Text[i + j] != Pattern[j])
+            {
+                break;
+            }
+        }
+
+        if (j >= PatternSize)
+        {
+            return i;
+        }
+
+    }
+    
+    return -1;   
+}
