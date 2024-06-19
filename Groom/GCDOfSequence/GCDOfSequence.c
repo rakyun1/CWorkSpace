047d11a48ca80bbc804625c6180788f7be170a21
rakyun1 <dbskim234@naver.com>
2024-06-19 15:51:26 +0900

    구름 : 수열의 최대공약수 uncompleted

---
+++ Groom/GCDOfSequence/GCDOfSequence.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+int main(void)
+{
+	int arrLength;
+	scanf("%d", &arrLength);
+
+	int arr[arrLength];
+	
+	for (int i = 0; i < arrLength; i++) {
+		scanf("%d", &arr[i]);
+	}
+	
+	int maxValue = 0;
+	for (int i = 0; i < arrLength; i++) {
+		if (arr[i] > maxValue) maxValue = arr[i];
+	}
+		
+	int currentGCD = 1;
+	int answer = INT_MAX;
+
+	while (currentGCD <= maxValue / 2)
+	{
+		int deletedNumCount = 0;
+		currentGCD++;
+
+		if (currentGCD > 3)
+		{
+			if (currentGCD % 2 == 0 || currentGCD % 3 == 0)
+			{
+				continue;
+			}
+		}
+
+		for (int i = 0; i < arrLength; i++)
+		{
+			if (arr[i] % currentGCD != 0)
+			{
+				deletedNumCount++;
+			}	
+		}
+		
+		if (deletedNumCount != 0 && deletedNumCount < arrLength && deletedNumCount < answer)
+		{
+			answer = deletedNumCount;
+		}
+
+	}
+	
+	// printf("GCD : %d\n", currentGCD);
+
+	if (answer == INT_MAX) printf("-1\n");
+	else
+	{
+		printf("%d\n", answer);
+	}
+
+	return 0;
+}
